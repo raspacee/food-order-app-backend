f7597d63e4e82b2749d18dd3c607d4334d308d04
Bijay Khapung <khapungbj84@gmail.com>
2024-07-10 17:11:47 +0545

    Add restaurant creation route

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,17 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoutes";
+import myRestaurantRoute from "./routes/MyRestaurantRoutes";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Database connected");
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
@@ -21,6 +29,7 @@ app.get("/health", (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server started on port 7000");
